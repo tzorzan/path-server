8a60e1311d64c69dccd055807cd9178e0c47f851
tzorzan <tobia.zorzan@gmail.com>
2014-08-07 22:08:12 +0200

    Map Matching step 1 fixed.

--- app/controllers/MapMatching.java
+++ app/controllers/MapMatching.java
@@ -16,33 +16,56 @@ import play.db.jpa.JPA;
 import play.mvc.Controller;
 import utils.OverpassQuery;
 
+import javax.persistence.Query;
+
 public class MapMatching extends Controller {
+    public static final Double toleranceMeters = 20.0;
+    private static final String bboxQuery = "" +
+            "SELECT" +
+            "    min(boundingbox.minlat) as minlat," +
+            "    min(boundingbox.minlon) as minlon," +
+            "    max(boundingbox.maxlat) as maxlat," +
+            "    max(boundingbox.maxlon) as maxlon " +
+            "FROM (" +
+            "    SELECT" +
+            "        boxes.id as id," +
+            "        ST_YMin(boxes.box) as minlat," +
+            "        ST_XMin(boxes.box) as minlon," +
+            "        ST_YMax(boxes.box) as maxlat," +
+            "        ST_XMax(boxes.box) as maxlon" +
+            "    FROM" +
+            "        (SELECT" +
+            "            ST_Transform(ST_Expand(ST_Transform(ST_SetSRID(ST_Point(s.longitude, s.latitude),4326), 2163), :tolerance), 4326) as box," +
+            "            s.id AS id" +
+            "        FROM" +
+            "            sample AS s" +
+            "        WHERE" +
+            "            s.path_id=:pathid" +
+            "        ) AS boxes" +
+            ") AS boundingbox;";
+
     public static void step1() {
     	Sample sample = Sample.find("byLoaded", false).first();
     	Path path = sample.path;
- 
+
     	List<Sample> samples = path.samples;
 
-            Segment s = new Segment();
-            GeometryFactory fact = new GeometryFactory();
-            Coordinate[] coords  =
-                    new Coordinate[] {new Coordinate(0, 2), new Coordinate(2, 0), new Coordinate(8, 6) };
-            s.linestring = fact.createLineString(coords);
+        Query query = JPA.em().createNativeQuery(bboxQuery).setParameter("tolerance", toleranceMeters).setParameter("pathid", path.id);
+        Object[] res = (Object[]) query.getResultList().get(0);
 
-            //Segment s2 = Segment.findById(61l);
-            //Logger.info("Segmento: " + s2.linestring.toText());
-            //s.save();
+        Logger.info(res[0] + " " + res[1]);
 
-        String query = "[out:json];\n" +
-                "( way\n" +
-                " (45.413678,11.813972,45.414751,11.817132)\n" +
-                "  [highway];\n" +
-                "  >; );\n" +
-                "out \n" +
+        String geoquery = "[out:json];" +
+                "( way" +
+                " ("+res[0]+","+res[1]+","+res[2]+","+res[3]+")" +
+                "  [highway];" +
+                "  >; );" +
+                "out " +
                 "  body;";
 
-        OverpassResponse r = new OverpassQuery(query).query();
+        OverpassResponse r = new OverpassQuery(geoquery).query();
 
+        GeometryFactory fact = new GeometryFactory();
         List<LineString> segments = new ArrayList<LineString>();
 
         for(OverpassResponse.Element e : r.getWays()) {
@@ -57,10 +80,16 @@ public class MapMatching extends Controller {
             segments.add(fact.createLineString(points.toArray(new Coordinate[points.size()])));
         }
 
-        Envelope boundingbox = new Envelope(45.414751, 45.413678, 11.813972, 11.817132);
+        Envelope boundingbox = new Envelope((Double) res[0],(Double) res[2],(Double) res[3],(Double) res[1]);
         render(samples, boundingbox, segments);
 
 /*
+SELECT
+	min(boundingbox.minlat) as minlat,
+	min(boundingbox.minlon) as minlon,
+	max(boundingbox.maxlat) as maxlat,
+	max(boundingbox.maxlon) as maxlon
+FROM (
 SELECT
     boxes.id as id,
     ST_YMin(boxes.box) as minlat,
@@ -68,15 +97,15 @@ SELECT
     ST_YMax(boxes.box) as maxlat,
     ST_XMax(boxes.box) as maxlon
 FROM
-    (SELECT    
-        ST_Transform(ST_Expand(ST_Transform(ST_SetSRID(ST_Point(s.longitude, s.latitude),4326), 2163), 20), 4326) as box, 
-        s.id AS id 
-    FROM 
+    (SELECT
+        ST_Transform(ST_Expand(ST_Transform(ST_SetSRID(ST_Point(s.longitude, s.latitude),4326), 2163), 20), 4326) as box,
+        s.id AS id
+    FROM
         sample AS s
     WHERE
 	s.loaded=false
-    ) AS boxes 
-ORDER BY id;         
+    ) AS boxes
+ ) AS boundingbox;
 */
         
 /*
@@ -95,6 +124,16 @@ out
   body;
 
 */
+
+        //Segment s = new Segment();
+        //GeometryFactory fact = new GeometryFactory();
+        //Coordinate[] coords  =
+        //        new Coordinate[] {new Coordinate(0, 2), new Coordinate(2, 0), new Coordinate(8, 6) };
+        //s.linestring = fact.createLineString(coords);
+
+        //Segment s2 = Segment.findById(61l);
+        //Logger.info("Segmento: " + s2.linestring.toText());
+        //s.save();
 
     }
 }
