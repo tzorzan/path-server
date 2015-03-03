100be65dfc0747a9268ef72d34549099e24f6f3a
tzorzan <tobia.zorzan@gmail.com>
2015-03-03 22:58:16 +0100

    Auto update noded network and topology (PGRouting functions).

---
+++ app/jobs/UpdateNetworkEdges.java
@@ -0,0 +1,29 @@
+package jobs;
+
+import play.Logger;
+import play.db.jpa.JPA;
+import play.jobs.Every;
+import play.jobs.Job;
+
+import javax.persistence.Query;
+
+@Every("150s")
+public class UpdateNetworkEdges extends Job {
+  public static final Double tolerance = 0.00001;
+	public void doJob() {
+        /*
+         *  Aggiorno le tabelle che rappresentano la Noded Network e la Network Topology
+         */
+
+        Logger.info("Updating network edges...");
+        Query NodeNetworkQuery = JPA.em().createNativeQuery("SELECT pgr_nodeNetwork('roadsegment', :tolerance, 'id', 'linestring');").setParameter("tolerance", tolerance);
+        Query CreateTopologyQuery = JPA.em().createNativeQuery("SELECT pgr_createTopology('roadsegment_noded', :tolerance, 'linestring');").setParameter("tolerance", tolerance);
+
+        String result = (String) NodeNetworkQuery.getSingleResult();
+        Logger.info("pgr_nodeNetwork: " + result);
+
+        result = (String) CreateTopologyQuery.getSingleResult();
+        Logger.info("pgr_createTopology: " + result);
+
+    }
+}
