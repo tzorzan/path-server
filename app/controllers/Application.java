3e9295cbdcc335cf1483b7890c167ba163d8c24f
tzorzan <tobia.zorzan@gmail.com>
2015-07-31 23:00:32 +0200

    Added route view page.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,26 @@
 package controllers;
 
-import play.*;
+import com.google.gson.Gson;
+import implementations.SPDRouter;
+import models.boundaries.PathRoutes;
 import play.mvc.*;
-
-import java.util.*;
-
-import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
+    }
+
+    public static void route() {
+        String[] from = params.get("from").split(",");
+        String[] to = params.get("to").split(",");
+
+        PathRoutes routes = new PathRoutes();
+        PathRoutes.Feature spdRoute = new SPDRouter().getRoute(from, to);
+
+        String geoJson = new Gson().toJson(spdRoute);
+
+        render(geoJson);
     }
 
 }
